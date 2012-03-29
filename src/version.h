99b768fa43e75e323b07d8172a92acefb06b173f
Anthony G. Basile <blueness@gentoo.org>
2012-03-29 13:26:02 -0400

    Revamp of the build system - continued

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define SERVER_SOFTWARE "thttpd/2.25b 29dec2003"
-#define SERVER_ADDRESS "http://www.acme.com/software/thttpd/"
+#define SERVER_SOFTWARE "thttpd/2.26 April 1, 2012"
+#define SERVER_ADDRESS "http://dev.gentoo.org/~blueness/sthttpd"
 
 #endif /* _VERSION_H_ */
