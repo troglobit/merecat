93f9b143c894ee40f3e338804c585bdad5c85f19
Anthony G. Basile <blueness@gentoo.org>
2012-04-03 14:37:56 -0400

    Beginning next major release

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define SERVER_SOFTWARE "thttpd/2.26 April 3, 2012"
+#define SERVER_SOFTWARE "thttpd/2.27 April 3, 2012"
 #define SERVER_ADDRESS "http://localhost"
 
 #endif /* _VERSION_H_ */
