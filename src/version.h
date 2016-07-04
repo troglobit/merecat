f227e99e408b09a9dc194c941d9d359b6899c9a9
Joachim Nilsson <troglobit@gmail.com>
2016-07-04 19:09:41 +0200

    Further rebranding of project sthttpd --> merecat
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define SERVER_SOFTWARE "sthttpd/2.27.0 03oct2014"
+#define SERVER_SOFTWARE "merecat/2.27.0 03oct2014"
 #define SERVER_ADDRESS "http://localhost"
 
 #endif /* _VERSION_H_ */
