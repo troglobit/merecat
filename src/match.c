16d22ae6ff20d06625f1e0efd40d2ae4e3ea2205
Joachim Nilsson <troglobit@gmail.com>
2017-02-11 15:48:48 +0100

    Minor coding style cleanup
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/match.c
+++ src/match.c
@@ -43,10 +43,12 @@ int match(const char *pattern, const char *string)
 
 	for (;;) {
 		or = strchr(pattern, '|');
-		if (or == (char *)0)
+		if (!or)
 			return match_one(pattern, strlen(pattern), string);
+
 		if (match_one(pattern, or - pattern, string))
 			return 1;
+
 		pattern = or + 1;
 	}
 }
@@ -59,6 +61,7 @@ static int match_one(const char *pattern, int patternlen, const char *string)
 	for (p = pattern; p - pattern < patternlen; ++p, ++string) {
 		if (*p == '?' && *string != '\0')
 			continue;
+
 		if (*p == '*') {
 			int i, pl;
 
@@ -67,19 +70,26 @@ static int match_one(const char *pattern, int patternlen, const char *string)
 				/* Double-wildcard matches anything. */
 				++p;
 				i = strlen(string);
-			} else
+			} else {
 				/* Single-wildcard matches anything but slash. */
 				i = strcspn(string, "/");
+			}
+
 			pl = patternlen - (p - pattern);
-			for (; i >= 0; --i)
+			for (; i >= 0; --i) {
 				if (match_one(p, pl, &(string[i])))
 					return 1;
+			}
+
 			return 0;
 		}
+
 		if (*p != *string)
 			return 0;
 	}
+
 	if (*string == '\0')
 		return 1;
+
 	return 0;
 }
