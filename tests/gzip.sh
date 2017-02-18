64d00d18c2d81203465d3d159123c4b6784a7c26
Joachim Nilsson <troglobit@gmail.com>
2017-02-18 20:28:02 +0100

    tests: Change non-priviliged web server port
    The port 8080 is used already by one of my build servers, with a redir
    redirecting incoming traffic from port 80 to 8080.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/gzip.sh
+++ tests/gzip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # https://en.wikipedia.org/wiki/HTTP_compression
 
-curl -H "Accept-Encoding: gzip" -I http://localhost:8080/main.css 2>/dev/null |grep gzip
+curl -H "Accept-Encoding: gzip" -I http://localhost:8086/main.css 2>/dev/null |grep gzip
 
