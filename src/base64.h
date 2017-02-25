6d675ce079e1d469a0facf001f6fd12423cb3515
Joachim Nilsson <troglobit@gmail.com>
2017-02-25 19:14:31 +0100

    Refactor: break out base64 code to separate file
    Will soon replace b64 with another (public domain) implementation
    to be able to serve embedded .gif images for dir listings.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ src/base64.h
@@ -0,0 +1,9 @@
+/* Base-64 decoding */
+
+#ifndef BASE64_H_
+#define BASE64_H_
+
+int b64_decode(const char *str, unsigned char *space, int size);
+
+#endif /* BASE64_H_ */
+
--- src/libhttpd.c
+++ src/libhttpd.c
@@ -76,6 +76,7 @@
 extern char *crypt(const char *key, const char *setting);
 
 /* Local headers */
+#include "base64.h"
 #include "libhttpd.h"
 #include "match.h"
 #include "md5.h"
@@ -139,7 +140,6 @@ static int send_err_file(httpd_conn *hc, int status, char *title, const char *ex
 #endif
 #ifdef AUTH_FILE
 static void send_authenticate(httpd_conn *hc, char *realm);
-static int b64_decode(const char *str, unsigned char *space, int size);
 static int auth_check(httpd_conn *hc, char *dirname);
 static int auth_check2(httpd_conn *hc, char *dirname);
 #endif
@@ -1158,87 +1158,6 @@ static void send_authenticate(httpd_conn *hc, char *realm)
 	*/
 	if (hc->method == METHOD_POST)
 		hc->should_linger = 1;
-}
-
-
-/* Base-64 decoding.  This represents binary data as printable ASCII
-** characters.  Three 8-bit binary bytes are turned into four 6-bit
-** values, like so:
-**
-**   [11111111]  [22222222]  [33333333]
-**
-**   [111111] [112222] [222233] [333333]
-**
-** Then the 6-bit values are represented using the characters "A-Za-z0-9+/".
-*/
-
-static int b64_decode_table[256] = {
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* 00-0F */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* 10-1F */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,	/* 20-2F */
-	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,	/* 30-3F */
-	-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,	/* 40-4F */
-	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,	/* 50-5F */
-	-1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,	/* 60-6F */
-	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,	/* 70-7F */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* 80-8F */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* 90-9F */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* A0-AF */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* B0-BF */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* C0-CF */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* D0-DF */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,	/* E0-EF */
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1	/* F0-FF */
-};
-
-/* Do base-64 decoding on a string.  Ignore any non-base64 bytes.
-** Return the actual number of bytes generated.  The decoded size will
-** be at most 3/4 the size of the encoded, and may be smaller if there
-** are padding characters (blanks, newlines).
-*/
-static int b64_decode(const char *str, unsigned char *space, int size)
-{
-	const char *cp;
-	int space_idx, phase;
-	int d, prev_d = 0;
-	unsigned char c;
-
-	space_idx = 0;
-	phase = 0;
-	for (cp = str; *cp != '\0'; ++cp) {
-		d = b64_decode_table[(int)*cp];
-		if (d != -1) {
-			switch (phase) {
-			case 0:
-				++phase;
-				break;
-
-			case 1:
-				c = ((prev_d << 2) | ((d & 0x30) >> 4));
-				if (space_idx < size)
-					space[space_idx++] = c;
-				++phase;
-				break;
-
-			case 2:
-				c = (((prev_d & 0xf) << 4) | ((d & 0x3c) >> 2));
-				if (space_idx < size)
-					space[space_idx++] = c;
-				++phase;
-				break;
-
-			case 3:
-				c = (((prev_d & 0x03) << 6) | d);
-				if (space_idx < size)
-					space[space_idx++] = c;
-				phase = 0;
-				break;
-			}
-			prev_d = d;
-		}
-	}
-
-	return space_idx;
 }
 
 
