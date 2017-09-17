9b573e24d414ed66b67e9e5fcd9bcae871e6a8b3
Joachim Nilsson <troglobit@gmail.com>
2017-09-17 11:38:51 +0200

    run.sh: Add command to run flattened image
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Serve volume /srv/ftp with merecat httpd on host port 80
-docker run -dit --restart unless-stopped -h `hostname` -v /srv/ftp:/var/www -p 80:80 alpine/merecat
+docker run -dit --restart unless-stopped -h `hostname` \
+       -v /srv/ftp:/var/www -p 80:80 troglobit/merecat:latest merecat -n /var/www
+
