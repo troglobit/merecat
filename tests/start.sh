4f8014c9fa3bc716903eca0e245ac5bb5d1e08af
Joachim Nilsson <troglobit@gmail.com>
2018-02-04 20:23:28 +0100

    Adapt test scripts to be run from `make distcheck`
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/start.sh
+++ tests/start.sh
@@ -2,8 +2,15 @@
 
 cd ../www
 
-../src/merecat -n -p 8086 -l none -f ../merecat.conf &
+../src/merecat -n -p 8086 -l none -f ${srcdir}/../merecat.conf &
 echo $! >/tmp/merecat.test
+
+if [ ! -e main.css ]; then
+    cp ${srcdir}/../www/main.css .
+    cp ${srcdir}/../www/index.html .
+
+    echo "main.css index.html" >/tmp/merecat.files
+fi
 
 gzip -c main.css   > main.css.gz
 gzip -c index.html > index.html.gz
--- tests/stop.sh
+++ tests/stop.sh
@@ -2,6 +2,9 @@
 
 cd ../www
 rm *.gz
-
+if [ -e /tmp/merecat.files ]; then
+    rm -f `cat /tmp/merecat.files`
+fi
 kill `cat /tmp/merecat.test`
 
+rm /tmp/merecat.test /tmp/merecat.files
