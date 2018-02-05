b158816905ce486603e47adbcc5a66b7c225f294
Joachim Nilsson <troglobit@gmail.com>
2018-02-05 07:50:11 +0100

    Fix minor regression in test suite harness
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/stop.sh
+++ tests/stop.sh
@@ -4,7 +4,8 @@ cd ../www
 rm *.gz
 if [ -e /tmp/merecat.files ]; then
     rm -f `cat /tmp/merecat.files`
+    rm /tmp/merecat.files
 fi
-kill `cat /tmp/merecat.test`
 
-rm /tmp/merecat.test /tmp/merecat.files
+kill `cat /tmp/merecat.test`
+rm /tmp/merecat.test
