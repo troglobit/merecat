troglobit/merecat

commit ba39ed9147d2fb6dfe6763aacfacb26b6b44654d

libhttpd: New method httpd_aton() validate and convert IP address
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/libhttpd.c
@@ -4759,6 +4759,32 @@ char *httpd_ntoa(sockaddr_t *sa)
 #endif
 }
 
+/* If sa is NULL this function can be used to validate the address */
+int httpd_aton(char *address, sockaddr_t *sa)
+{
+	struct addrinfo *res = NULL;
+	struct addrinfo hints;
+	sockaddr_t local;
+	int rc;
+
+	if (!sa)
+		sa = &local;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family   = PF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags    = AI_NUMERICHOST | AI_NUMERICSERV;
+
+	rc = getaddrinfo(address, NULL, &hints, &res);
+	if (rc != 0 || !res)
+		return -1;
+
+	memcpy(&sa->sa, res->ai_addr, sizeof(sa->sa));
+	httpd_ntoa(sa);
+
+	return 0;
+}
+
 short httpd_port(sockaddr_t *sa)
 {
 #ifdef USE_IPV6

src/libhttpd.h
@@ -371,6 +371,9 @@ extern void httpd_realloc_str(char **str, size_t *curr_len, size_t new_len);
 /* Format a network socket to a string representation. */
 extern char *httpd_ntoa(sockaddr_t *sa);
 
+/* Convert string address to socket address */
+extern int httpd_aton(char *address, sockaddr_t *sa);
+
 /* Return port from sockaddr */
 extern short httpd_port(sockaddr_t *sa);
 

commit aa15e577cbdc56d0954dfedd3b4eb584de07c169

Fix memory leak in httpd_aton(), introduced in ba39ed9
Found by Coverity Scan.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/libhttpd.c
@@ -4797,6 +4797,9 @@ int httpd_aton(char *address, sockaddr_t *sa)
 		return -1;
 
 	memcpy(&sa->sa, res->ai_addr, sizeof(sa->sa));
+	freeaddrinfo(res);
+
+	/* Fill in sa->address, which should be same as address */
 	httpd_ntoa(sa);
 
 	return 0;