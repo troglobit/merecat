troglobit/merecat

commit b865d2dcc81c0d36e8be4c6e846a9a1fe0707a11

Built-in icons for simple dir listings
This patch adds a few built-in icons to Merecat.  Useful for simple file
listings where the icons are outside the server path.  Works better than
the fallback unicode characters in all browsers.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/mmc.c
@@ -111,10 +111,100 @@ static int add_hash(Map *m);
 static Map *find_hash(ino_t ino, dev_t dev, off_t size, time_t ctime);
 static unsigned int hash(ino_t ino, dev_t dev, off_t size, time_t ctime);
 
+#include "base64.h"
+
+struct {
+	char *name;
+	char *buf;
+} icons[] = {
+	{
+		"icons/back.gif",
+		"R0lGODlhFAAWAMIAAP///8z//5mZmWZmZjMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRo"
+		"ZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIg"
+		"MTk5NQAh+QQBAAABACwAAAAAFAAWAAADSxi63P4jEPJqEDNTu6LO3PVpnDdOFnaCkHQGBTcqRRxu"
+		"WG0v+5LrNUZQ8QPqeMakkaZsFihOpyDajMCoOoJAGNVWkt7QVfzokc+LBAA7"
+	},
+	{
+		"icons/blank.gif",
+		"R0lGODlhFAAWAKEAAP///8z//wAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9t"
+		"YWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAAB"
+		"ACwAAAAAFAAWAAACE4yPqcvtD6OctNqLs968+w+GSQEAOw=="
+	},
+	{
+		"icons/folder.gif",
+		"R0lGODlhFAAWAMIAAP/////Mmcz//5lmMzMzMwAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRo"
+		"ZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIg"
+		"MTk5NQAh+QQBAAACACwAAAAAFAAWAAADVCi63P4wyklZufjOErrvRcR9ZKYpxUB6aokGQyzHKxyO"
+		"9RoTV54PPJyPBewNSUXhcWc8soJOIjTaSVJhVphWxd3CeILUbDwmgMPmtHrNIyxM8Iw7AQA7"
+	},
+	{
+		"icons/generic.gif",
+		"R0lGODlhFAAWAMIAAP///8z//5mZmTMzMwAAAAAAAAAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRo"
+		"ZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIg"
+		"MTk5NQAh+QQBAAABACwAAAAAFAAWAAADUDi6vPEwDECrnSO+aTvPEddVIriN1wWJKDG48IlSRG0T"
+		"8kwJvIBLOkvvxwoCfDnjkaisIIHNZdL4LAarUSm0iY12uUwvcdArm3mvyG3N/iUAADs="
+	}
+};
+
+static struct stat icost;
+static int  cico = -1;
+static unsigned char icon[512];	/* Only small icons allowed atm */
+
+int mmc_icon_check(char *filename, struct stat *st)
+{
+	char *ptr;
+	size_t i;
+
+	cico = -1;
+	ptr = strstr(filename, "icons/");
+	if (!ptr)
+		return 0;
+
+	for (i = 0; i < NELEMS(icons); i++) {
+		off_t len;
+
+		if (strcmp(ptr, icons[i].name))
+			continue;
+
+		len = b64_decode(icons[i].buf, icon, sizeof(icon));
+		if (len <= 0)
+			break;
+
+		memset(&icost, 0, sizeof(icost));
+		icost.st_size = len;
+		icost.st_ctim.tv_sec = 18446744073359756536UL;
+		if (st)
+			memcpy(st, &icost, sizeof(icost));
+
+		cico = i;
+		return 1;
+	}
+
+	return 0;
+}
+
+/* Check if this is a small icon we have built-in */
+static off_t mmc_icon_open(char *filename, char **buf, struct stat *st)
+{
+	int found = 1;
+
+	if (cico < 0 || cico > (int)NELEMS(icons) || strcmp(icons[cico].name, filename))
+		found = mmc_icon_check(filename, st);
+
+	if (!found)
+		return -1;
+
+	*buf = (char *)icon;
+	*st = icost;
+
+	return 0;
+}
+
 
 void *mmc_map(char *filename, struct stat *sbP, struct timeval *nowP)
 {
 	time_t now;
+	char *buf = NULL;
 	struct stat sb;
 	Map *m;
 	int fd;
@@ -153,8 +243,10 @@ void *mmc_map(char *filename, struct stat *sbP, struct timeval *nowP)
 	/* Open the file. */
 	fd = open(filename, O_RDONLY);
 	if (fd < 0) {
-		syslog(LOG_ERR, "open: %s", strerror(errno));
-		return NULL;
+		if (mmc_icon_open(filename, &buf, &sb)) {
+			syslog(LOG_ERR, "open: %s", strerror(errno));
+			return NULL;
+		}
 	}
 
 	/* Find a free Map entry or make a new one. */
@@ -165,7 +257,8 @@ void *mmc_map(char *filename, struct stat *sbP, struct timeval *nowP)
 	} else {
 		m = (Map *)malloc(sizeof(Map));
 		if (!m) {
-			close(fd);
+			if (!buf)
+				close(fd);
 			syslog(LOG_ERR, "out of memory allocating a Map");
 
 			return NULL;
@@ -190,6 +283,17 @@ void *mmc_map(char *filename, struct stat *sbP, struct timeval *nowP)
 	} else {
 		size_t size_size = (size_t)m->size;	/* loses on files >2GB */
 
+		if (buf) {
+			m->addr = malloc(size_size);
+			if (!m->addr) {
+				syslog(LOG_ERR, "%s: %s", __func__, strerror(errno));
+				free(m);
+				--alloc_count;
+				return NULL;
+			}
+			memcpy(m->addr, buf, size_size);
+			goto cont;
+		}
 #ifdef HAVE_MMAP
 		/* Map the file into memory. */
 		m->addr = mmap(0, size_size, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -241,7 +345,7 @@ void *mmc_map(char *filename, struct stat *sbP, struct timeval *nowP)
 #endif /* HAVE_MMAP */
 	}
 	close(fd);
-
+cont:
 	/* Put the Map into the hash table. */
 	if (add_hash(m) < 0) {
 		syslog(LOG_ERR, "add_hash() failure");

src/libhttpd.c
@@ -3919,6 +3919,7 @@ static int really_start_request(httpd_conn *hc, struct timeval *nowP)
 	char *dirname = NULL;
 	size_t maxdirname = 0;
 #endif
+	int is_icon;
 	char *cp, *pi;
 	static const char *index_names[] = { INDEX_NAMES };
 	size_t expnlen, indxlen, i;
@@ -3928,6 +3929,13 @@ static int really_start_request(httpd_conn *hc, struct timeval *nowP)
 	if (hc->method != METHOD_GET && hc->method != METHOD_HEAD && hc->method != METHOD_POST) {
 		httpd_send_err(hc, 501, err501title, "", err501form, httpd_method_str(hc->method));
 		return -1;
+	}
+
+	is_icon = mmc_icon_check(hc->pathinfo, &hc->sb);
+	if (is_icon) {
+		strcpy(hc->expnfilename, hc->pathinfo);
+		strcpy(hc->pathinfo, "");
+		goto sneaky;
 	}
 
 	/* Stat the file. */
@@ -4119,6 +4127,7 @@ static int really_start_request(httpd_conn *hc, struct timeval *nowP)
 	}
 #endif /* AUTH_FILE */
 
+sneaky:
 	/* Referer check. */
 	if (!check_referer(hc))
 		return -1;
@@ -4174,7 +4183,10 @@ static int really_start_request(httpd_conn *hc, struct timeval *nowP)
 
 		hc->file_address = mmc_map(hc->expnfilename, &(hc->sb), nowP);
 		if (!hc->file_address) {
-			httpd_send_err(hc, 500, err500title, "", err500form, hc->encodedurl);
+			if (is_icon)
+				httpd_send_err(hc, 404, err404title, "", err404form, hc->encodedurl);
+			else
+				httpd_send_err(hc, 500, err500title, "", err500form, hc->encodedurl);
 			return -1;
 		}
 

src/mmc.h
@@ -28,6 +28,9 @@
 #ifndef _MMC_H_
 #define _MMC_H_
 
+/* Checks if filename is a built-in icon */
+extern int mmc_icon_check(char *filename, struct stat *st);
+
 /* Returns an mmap()ed area for the given file, or (void*) 0 on errors.
 ** If you have a stat buffer on the file, pass it in, otherwise pass 0.
 ** Same for the current time.

commit 4c5dffd84ccfd7e7ed849629c470bed0d6e1ceb8

mmc: Only real files need munmap(), built-in icons we free()
This fixes a memory leak introduced in b865d2d, and also closes the
annoying syslog error:

    merecat[32448]: munmap(): Invalid argument

Only actual mmap()'ed files should be munmap()'ed, but since b865d2dcc
we use mmc.c to wrap built-in icons to emulate real files, which are
not files, but local base64 encoded arrays.

Note: we probably don't need to malloc()/free() this per connection,
      but that's a future TODO.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/mmc.c
@@ -448,12 +448,19 @@ static void panic(void)
 static void really_unmap(struct map **mm)
 {
 	struct map *m;
+	int rc = 0;
 
 	m = *mm;
 	if (m->size > 0) {
-		if (munmap(m->addr, m->size) < 0)
-			syslog(LOG_ERR, "munmap(%p, %td): %s", m->addr, m->size, strerror(errno));
+		/* Only real files have an inote, free built-in icon data */
+		if (!m->ino)
+			free(m->addr);
+		else
+			rc = munmap(m->addr, m->size);
 	}
+
+	if (rc < 0)
+		syslog(LOG_ERR, "munmap(%p, %td): %s", m->addr, m->size, strerror(errno));
 
 	/* Update the total byte count. */
 	mapped_bytes -= m->size;