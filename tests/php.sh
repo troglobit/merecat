086522e2f53783e49f143dc92bb13a89f2b018eb
Joachim Wiberg <troglobit@gmail.com>
2026-03-14 08:58:31 +0100

    tests: Skip if php-cgi is not installed
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- tests/php.sh
+++ tests/php.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -ex
 
+# Skip if php-cgi is not installed
+command -v php-cgi >/dev/null 2>&1 || exit 77
+
 echo "<?php echo 'Hello ' . htmlspecialchars(\$_GET[\"name\"]) . '!'; ?>" >srv/test.php
 ls srv
 cat srv/test.php
